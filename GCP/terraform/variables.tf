b02b5a7a55ce7d23c84f53a91ae7b6f920c26dd7
Dmitry Zhuravlev <zhdm.dev@gmail.com>
2025-12-08 15:21:00 -0500

    create GCP/terraform/main.tf

---
+++ GCP/terraform/variables.tf
@@ -0,0 +1,34 @@
+variable "project_id" {
+  description = "The ID of the GCP project"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "The GCP zone"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "instance_type" {
+  description = "The machine type for the instances"
+  type        = string
+  default     = "e2-micro"
+}
+
+variable "image_family" {
+  description = "The image family for the OS"
+  type        = string
+  default     = "debian-11"
+}
+
+variable "image_project" {
+  description = "The project for the OS image"
+  type        = string
+  default     = "debian-cloud"
+}
